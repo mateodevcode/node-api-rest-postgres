14eabd9723d4e13c0e0166528ab12e2e5e02d6a7
Mateo lizcano <mateodevcode@gmail.com>
2025-12-25 10:35:55 +0100

    se actualizo el health

--- src/routes/health.routes.js
+++ src/routes/health.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/health", (req, res) => {
-  res.json({ message: "Server is running" });
+  res.status(200).json({ message: "Server is running" });
 });
 
 export default router;
